a4369fd79be7fb5e34a87a2e195b272fe08733bb
John Chen <jglchen@gmail.com>
2023-02-11 21:12:53 +0800

    Update layoyt.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,6 +6,15 @@ export default function Layout({ children}: {children: JSX.Element}) {
            <Head>
              <title>Chat Applications with Cloud Firestore</title>
              <link rel="icon" href="/favicon.ico" />
+             <meta
+              name="description"
+              content="A real-time chat application with Firebase Cloud FireStore"
+              />
+             <meta name="og:title" content="Chat Applications with Cloud Firestore" />
+             <meta
+              property="og:description"
+              content="A real-time chat application with Firebase Cloud FireStore"
+              />
            </Head>
            <main>
               {children}
